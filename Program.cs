fd6f7428f7d1e25b93d8aa1dcb8d7c9dc60c5adb
nokinsddd <nk.sorokin98@gmail.com>
2026-05-28 05:29:54 +0700

    Added scalar

--- Program.cs
+++ Program.cs
@@ -1,15 +1,27 @@
 using GameStore.Api.Data;
 using GameStore.Api.EndPoints;
+using Scalar.AspNetCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddValidation();
 builder.AddGameStoreDb();
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+Console.WriteLine($">>>Environment: {app.Environment.EnvironmentName}<<<");
+
 app.MapGamesEndpoints();
 app.MapGenresEndpoints();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();              // Generates /openapi/v1.json
+    app.MapScalarApiReference();   // Serves docs at /scalar
+}
+
 
 app.MigrateDb();
 
